a4c0262af4677da481de3c3eb61bbe31f7c7ea73
Tim Smith <tsmith@chef.io>
2017-02-21 14:25:07 -0800

    Expand the requirements section of the readme
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,15 @@
 #
-# Cookbook Name:: iis_urlrewrite
+# Cookbook:: iis_urlrewrite
 # Attributes:: default
 # Author:: Julian C. Dunn (<jdunn@getchef.com>)
-# Copyright:: (C) 2014 Chef Software, Inc.
-# 
+# Copyright:: 2014-2017, Chef Software, Inc.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
