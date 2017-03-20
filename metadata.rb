63cef644712f902b43887928d538b1d7d4c27625
Tim Smith <tsmith@chef.io>
2017-03-20 13:07:56 -0700

    Fix the metadata URLs and require Chef 12.6+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'iis_urlrewrite'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures the IIS URL Rewrite Module'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.1'
@@ -9,6 +9,6 @@ version          '2.0.1'
 supports 'windows'
 depends  'iis'
 
-source_url 'https://github.com/chef-windows/iis_urlrewrite'
-issues_url 'https://github.com/chef-windows/iis_urlrewrite/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url 'https://github.com/chef-cookbooks/iis_urlrewrite'
+issues_url 'https://github.com/chef-cookbooks/iis_urlrewrite/issues'
+chef_version '>= 12.6' if respond_to?(:chef_version)
