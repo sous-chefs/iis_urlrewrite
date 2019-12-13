3aa370b1617af1d44f57962c6e04a9724a257019
Tim Smith <tsmith84@gmail.com>
2019-12-12 16:22:18 -0800

    Remove metadata.rb long_description field
    This is unused
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs and configures the IIS URL Rewrite Module'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.1.0'
 
 supports 'windows'
